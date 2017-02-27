69e002241283e3ef5af864ea0b5a13becf0b7576
Mohamed Huzien <muhammeda578@gmail.com>
2017-02-27 22:36:17 +0200

    add tools

---
+++ ubuntu_tools.sh
@@ -0,0 +1,85 @@
+
+sudo apt-get update
+
+
+echo '...............................................................'
+
+echo 'installing python pip ..... '
+echo '...............................................................'
+sudo apt-get install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+
+
+echo '...............................................................'
+
+echo 'installing Flask pyhton Microframework ....'
+
+echo '...............................................................'
+sudo pip install Flask
+
+
+
+echo '.................................................................. '
+
+echo 'installing machine learing tools  ................................'
+
+echo '.................................................................. '
+
+
+
+
+
+echo '.................................................................. '
+
+echo 'installing machine learning tools  , installing tensorflow ...'
+echo '.................................................................. '
+
+sudo pip install -U tensorflow
+
+
+
+echo '.................................................................. '
+
+echo 'installing machine learing tools , installing scikit-learn .... '
+echo '.................................................................. '
+
+sudo pip install -U scikit-learn
+
+
+echo '.................................................................. ......................'
+
+echo 'installing machine learing , installing  numpy scipy jupyter pandas sympy nose .... .... '
+echo '.......................................................................................... '
+
+sudo pip install -U numpy scipy jupyter pandas sympy nose
+
+
+
+echo '.................................................................. '
+
+echo 'installing machine learing , installing nltk ............... ......'
+echo '.................................................................. '
+sudo pip install -U nltk
+
+
+
+echo '.................................................................. '
+
+echo 'installing database tools ..,'
+echo '.................................................................. '
+
+
+
+echo '.................................................................. '
+
+echo 'installing python-mysqldb ..,'
+echo '.................................................................. '
+sudo apt-get install python-mysqldb
+
+
+echo '.................................................................. '
+
+echo 'install Python MongoDB driver ... ..............................'
+echo '.................................................................. '
+sudo python -m pip install pymongo
+
